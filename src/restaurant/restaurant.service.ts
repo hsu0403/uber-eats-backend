7072738a9f1acf689ae4586f205603ec92dae8da
SeongUn <hseongun0403@gmail.com>
2022-10-23 20:20:57 +0900

    fix query

--- src/restaurant/restaurant.service.ts
+++ src/restaurant/restaurant.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
-import { Raw, Repository } from 'typeorm';
+import { MoreThan, Raw, Repository } from 'typeorm';
 import { AllCategoriesOutput } from './dtos/all-categories.dto';
 import { CategoryInput, CategoryOutput } from './dtos/category.dto';
 import { CreateDishInput, CreateDishOutput } from './dtos/create-dish.dto';
@@ -223,23 +223,13 @@ export class RestaurantService {
     { id }: MyRestaurantInput,
   ): Promise<MyRestaurantOutput> {
     try {
-      const today = new Date();
-      const year = today.getFullYear().toString();
-      const month = (today.getMonth() + 1).toString();
-      const date = year.concat(`-${month}-01`);
       const restaurant = await this.restaurants.findOne({
         where: {
           owner: { id: owner.id },
           id,
-          orders: {
-            createdAt: Raw((created) => `${created} >= :date`, {
-              date,
-            }),
-          },
         },
         relations: ['menu', 'orders'],
       });
-
       return {
         restaurant,
         ok: true,
